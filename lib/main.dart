e612ce06970236c1d1dafcd63ae5c91ea07e971a
kimminki <hungry87@hanmi.co.kr>
2023-02-18 23:59:27 +0900

    ing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:actual/user/view/login_screen.dart';
+import 'package:actual/common/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class _App extends StatelessWidget {
         fontFamily: 'NotoSans',
       ),
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
+      home: const SplashScreen(),
     );
   }
 }

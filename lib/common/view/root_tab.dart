a160644a433a3dc956c0ff6dfa7fb0a8a4913e64
kimminki <hungry87@hanmi.co.kr>
2023-02-20 01:05:59 +0900

    ing

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,14 +1,95 @@
-﻿import 'package:actual/common/layout/default_layout.dart';
+﻿import 'package:actual/common/const/colors.dart';
+import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/restaurant/view/restaurant_screen.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  int index = 0;
+  late TabController controller;
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    controller = TabController(length: 4, vsync: this);
+    controller.addListener(tabListener);
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      child: Center(
-        child: Text('Root tab'),
+    return DefaultLayout(
+      title: '코팩 딜리버리',
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: PRIMARY_COLOR,
+          unselectedItemColor: BODY_TEXT_COLOR,
+          selectedFontSize: 10,
+          unselectedFontSize: 10,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            // setState(() {
+            //   this.index = index;
+            // });
+            controller.animateTo(index);
+          },
+          currentIndex: index,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood_outlined),
+              label: '음식',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long_outlined),
+              label: '주문',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: '프로필',
+            ),
+          ]),
+      child: TabBarView(
+        physics: const NeverScrollableScrollPhysics(), //좌우측 스크롤 금지
+        controller: controller,
+        children: [
+          const RestaurantScreen(),
+          Center(
+            child: Container(
+              child: const Text('음식'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: const Text('주문'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: const Text('프로필'),
+            ),
+          ),
+        ],
       ),
     );
   }

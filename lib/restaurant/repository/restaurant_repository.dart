696009c6f9efb678a2811e160d5b1a26699eecb2
kimminki <hungry87@hanmi.co.kr>
2023-02-22 15:32:09 +0900

    finish

--- lib/restaurant/repository/restaurant_repository.dart
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -15,7 +15,9 @@ abstract class RestaurantRepository {
   // paginate();
 
   // http://$ip/restaurant/:id
-  // 일정 시간이 지나면 토큰값이 변경되므로 Postman에서 발급받아 변경 후 테스트
+  // 일정 시간이 지나면 토큰값이 변경되므로 Postman에서 발급받아 변경 후 테스트 한다.
+  // 그리고 flutter pub run build_runner build로 다시 빌드해야 retrofit으로 생성한
+  // restaurant_repository.g.dart 내부에 authorization 값도 같이 갱신되어서 테스트 가능
   @GET('/{id}')
   @Headers({
     'authorization':

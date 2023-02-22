a65669b1350b33176bebdf615b6d5d3e11a673d8
kimminki <hungry87@hanmi.co.kr>
2023-02-22 15:13:22 +0900

    finish

---
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -0,0 +1,27 @@
+﻿import 'package:actual/restaurant/model/restaurant_detail_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+
+part 'restaurant_repository.g.dart';
+
+@RestApi()
+abstract class RestaurantRepository {
+  // http://$ip/restaurant
+  factory RestaurantRepository(Dio dio, {String baseUrl}) =
+      _RestaurantRepository;
+
+  // http://$ip/restaurant/
+  // @GET('/')
+  // paginate();
+
+  // http://$ip/restaurant/:id
+  // 일정 시간이 지나면 토큰값이 변경되므로 Postman에서 발급받아 변경 후 테스트
+  @GET('/{id}')
+  @Headers({
+    'authorization':
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjU4NzI2MzMyLCJleHAiOjE2NTg3MjY2MzJ9.oWsZ3IAnTIquc5CuUuZJ46WLktU5T2HIeElUW0aBrso'
+  })
+  Future<RestaurantDetailModel> getRestaurantDetail({
+    @Path() required String id,
+  });
+}

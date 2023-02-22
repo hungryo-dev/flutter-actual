8025775df5067227d39fd15212cfea26bd42fcbb
kimminki <hungry87@hanmi.co.kr>
2023-02-22 10:17:08 +0900

    finish

---
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -0,0 +1,26 @@
+﻿import 'package:actual/restaurant/model/restaurant_detail_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+
+part 'restaurant_repository.g.dart';
+
+@RestApi()
+abstract class RestaurantRepository {
+  // http://$ip/restaurant
+  factory RestaurantRepository(Dio dio, {String baseUrl}) =
+      _RestaurantRepository;
+
+  // http://$i>p/restaurant/
+  // @GET('/')
+  // paginate();
+
+  // http://$ip/restaurant/:id
+  @GET('/{id}')
+  @Headers({
+    'authorization':
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imh1bmdyeSIsInN1YiI6ImY1NWIzMmQyLTRkNjgtNGMxZS1hM2NhLWRhOWQ3ZDBkOTJlNSIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE2NzcwMjY3MDcsImV4cCI6MTY3NzAyNzAwN30.SALQAGi-4GE1q1lZeHcLvbAkBEYwU1x44AKHMP2Vf3M'
+  })
+  Future<RestaurantDetailModel> getRestaurantDetail({
+    @Path() required String id,
+  });
+}
--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:actual/common/layout/default_layout.dart';
 import 'package:actual/product/component/product_card.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_detail_model.dart';
+import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -10,46 +11,56 @@ class RestaurantDetailScreen extends StatelessWidget {
   final String id;
   const RestaurantDetailScreen({required this.id, super.key});
 
-  Future<Map<String, dynamic>> getRestaurantDetail() async {
+  Future<RestaurantDetailModel> getRestaurantDetail() async {
     final dio = Dio();
 
-    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+    // final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
 
-    final resp = await dio.get(
-      'http://$ip/restaurant/$id',
-      options: Options(
-        headers: {
-          'authorization': 'Bearer $accessToken',
-        },
-      ),
-    );
+    // final resp = await dio.get(
+    //   'http://$ip/restaurant/$id',
+    //   options: Options(
+    //     headers: {
+    //       'authorization': 'Bearer $accessToken',
+    //     },
+    //   ),
+    // );
+    // return resp.data;
 
-    print(resp);
-    return resp.data;
+    final repository =
+        RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant');
+
+    return repository.getRestaurantDetail(id: id);
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: FutureBuilder<Map<String, dynamic>>(
+      child: FutureBuilder(
           future: getRestaurantDetail(),
           builder: (_, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+
             if (!snapshot.hasData) {
               return const Center(
                 child: CircularProgressIndicator(),
               );
             }
 
-            final item = RestaurantDetailModel.fromJson(
-              snapshot.data!,
-            );
+            // final item = RestaurantDetailModel.fromJson(
+            //   snapshot.data!,
+            // );
 
             return CustomScrollView(
               slivers: [
-                renderTop(model: item),
+                // renderTop(model: item),
+                renderTop(model: snapshot.data!),
                 renderLabel(),
-                renderProducts(products: item.products),
+                renderProducts(products: snapshot.data!.products),
               ],
             );
           }),
